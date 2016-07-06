0045a58056bf8a3aa5dcee65ad544b61e1c7b9dc
paning <pwcn_78@163.com>
2016-07-06 15:56:09 +0800

    去掉部门对应关系

---
+++ core-parent/admin-core-biz/src/main/java/com/kalix/admin/core/util/Compare.java
@@ -0,0 +1,9 @@
+package com.kalix.admin.core.util;
+
+import java.util.Comparator;
+
+public class Compare {
+    public static <E> Comparator<E> compare() {
+        return (e1, e2) -> 0;
+    }
+}
