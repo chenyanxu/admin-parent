47942e9692e8121335a24c31785d559134161f4d
chenyanxu <yanxu2016>
2016-07-22 09:43:08 +0800

    new admin dict menu impl

--- core-parent/admin-core-web/src/main/java/com/kalix/admin/core/web/impl/menu/AdminDictMenuImpl.java
+++ core-parent/admin-core-web/src/main/java/com/kalix/admin/core/web/impl/menu/AdminDictMenuImpl.java
@@ -2,4 +2,4 @@ package com.kalix.admin.core.web.impl.menu;
 
 import com.kalix.framework.core.impl.web.BaseMenuImpl;
 
-public class DictMenuImpl extends BaseMenuImpl{}
+public class AdminDictMenuImpl extends BaseMenuImpl{}
