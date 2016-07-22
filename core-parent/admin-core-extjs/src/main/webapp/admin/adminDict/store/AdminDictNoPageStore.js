ed8d3a163279e3f3c881e5106d93ed13d1f2f71b
chenyanxu <yanxu2016>
2016-07-22 09:45:18 +0800

    apply new dict extjs

--- core-parent/admin-core-extjs/src/main/webapp/admin/adminDict/store/AdminDictNoPageStore.js
+++ core-parent/admin-core-extjs/src/main/webapp/admin/adminDict/store/AdminDictNoPageStore.js
@@ -1,9 +1,7 @@
 /**
  * 字典数据仓库
  *
- * @author majian <br/>
- *         date:2015-7-9
- * @version 1.0.0
+ * @author chenyanxu
  */
 Ext.define('kalix.admin.adminDict.store.AdminDictNoPageStore', {
     extend: 'kalix.store.BaseStore',
