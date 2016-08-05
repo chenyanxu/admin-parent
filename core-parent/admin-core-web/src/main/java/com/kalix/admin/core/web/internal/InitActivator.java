4cfe7c7b65ba00cd90f663a59219762c05feaa0c
chenyanxu <yanxu2016>
2016-08-05 17:26:14 +0800

    AdminApplicationConfig listener

--- core-parent/admin-core-web/src/main/java/com/kalix/admin/core/web/internal/InitActivator.java
+++ core-parent/admin-core-web/src/main/java/com/kalix/admin/core/web/internal/InitActivator.java
@@ -1,8 +1,15 @@
 package com.kalix.admin.core.web.internal;
 
+import com.kalix.framework.core.api.web.IApplication;
 import com.kalix.framework.core.util.SystemUtil;
+import org.apache.camel.impl.osgi.tracker.BundleTracker;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+
+import java.util.Dictionary;
+import java.util.Hashtable;
+
 /**
  * Created by sunlf on 14-3-23.
  */
@@ -15,5 +22,6 @@ public class InitActivator implements BundleActivator {
     @Override
     public void stop(BundleContext bundleContext) throws Exception {
         SystemUtil.stopBundlePrintln(bundleContext);
+        ServiceReference[] refs = bundleContext.getServiceReferences(IApplication.class.getName(), null);
     }
 }
