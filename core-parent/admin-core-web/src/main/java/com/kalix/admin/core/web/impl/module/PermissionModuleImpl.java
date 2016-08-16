d7b9943512308fa81e2562e1c7e36298c5600c25
chenyanxu <yanxu2016>
2016-08-16 08:49:37 +0800

    Now all the IAppliaciont/IModule/IMenu will auto register in Config Listeners,so we remove un use resources

--- core-parent/admin-core-web/src/main/java/com/kalix/admin/core/web/impl/module/PermissionModuleImpl.java
+++
@@ -1,9 +0,0 @@
-package com.kalix.admin.core.web.impl.module;
-
-import com.kalix.framework.core.impl.web.BaseModuleImpl;
-
-/**
- * @author chenyanxu
- */
-
-public class PermissionModuleImpl extends BaseModuleImpl{}
--- core-parent/admin-core-web/src/main/java/com/kalix/admin/core/web/internal/InitActivator.java
+++ core-parent/admin-core-web/src/main/java/com/kalix/admin/core/web/internal/InitActivator.java
@@ -1,15 +1,8 @@
 package com.kalix.admin.core.web.internal;
 
-import com.kalix.framework.core.api.web.IApplication;
 import com.kalix.framework.core.util.SystemUtil;
-import org.apache.camel.impl.osgi.tracker.BundleTracker;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceReference;
-
-import java.util.Dictionary;
-import java.util.Hashtable;
-
 /**
  * Created by sunlf on 14-3-23.
  */
@@ -22,6 +15,5 @@ public class InitActivator implements BundleActivator {
     @Override
     public void stop(BundleContext bundleContext) throws Exception {
         SystemUtil.stopBundlePrintln(bundleContext);
-        ServiceReference[] refs = bundleContext.getServiceReferences(IApplication.class.getName(), null);
     }
 }
