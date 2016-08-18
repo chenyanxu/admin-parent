427e1672e3a847040bdfd84b8e71ec69fdb27c3b
paning <pwcn_78@163.com>
2016-08-18 10:00:04 +0800

    修改用户的机构列表树，不指定store

--- core-parent/admin-core-extjs/src/main/webapp/admin/org/view/UserOrgTreeList.js
+++ core-parent/admin-core-extjs/src/main/webapp/admin/org/view/UserOrgTreeList.js
@@ -8,7 +8,6 @@ Ext.define('kalix.admin.org.view.UserOrgTreeList', {
     extend: 'kalix.view.components.common.BaseTreeList',
     alias: 'widget.userorgtreelist',
     xtype: 'userorgtreelist',
-    store:  Ext.create('kalix.admin.org.store.UserOrgStore'),
     title: '机构列表',
     iconCls: 'iconfont icon-organization-management'
 });
