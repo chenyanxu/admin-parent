c4fe3209b4535947d3be82c38d447f287a768188
chenyanxu <yanxu2016>
2016-09-06 16:38:06 +0800

    1.add security bar to tree panel 2.apply auto height plugin to function main.js

--- core-parent/admin-core-extjs/src/main/webapp/sys/application/view/ApplicationTreeList.js
+++ core-parent/admin-core-extjs/src/main/webapp/sys/application/view/ApplicationTreeList.js
@@ -7,17 +7,15 @@
 Ext.define('kalix.admin.application.view.ApplicationTreeList', {
     extend: 'Ext.tree.Panel',
     requires: [
-        'kalix.admin.application.controller.ApplicationGridController'
+        'kalix.admin.application.controller.ApplicationGridController',
+        'kalix.plugin.AutoHeightPlugin'
     ],
+    plugins:['autoheightplugin'],
     alias: 'widget.applicationTreeList',
     xtype: 'applicationTreeList',
     controller: 'applicationGridController',
     collapsible: true,
     autoScroll: true,
     border: true,
-    /*rootProperty:{
-     id:'-1',
-     name:'根机构'
-     },*/
     rootVisible: false
 });
--- core-parent/admin-core-extjs/src/main/webapp/sys/function/view/FunctionGrid.js
+++ core-parent/admin-core-extjs/src/main/webapp/sys/function/view/FunctionGrid.js
@@ -5,86 +5,68 @@
  * @version 1.0.0
  */
 Ext.define('kalix.admin.function.view.FunctionGrid', {
-    extend: 'Ext.tree.Panel',
-    requires: [
-        'kalix.admin.function.viewModel.FunctionViewModel',
-        'kalix.admin.function.controller.FunctionGridController'
-    ],
-    alias: 'widget.functionGrid',
-    xtype: 'functionGridPanel',
-    controller: 'functionGridController',
-    viewModel: {
-        type: 'functionViewModel'
-    },
-    data: {
-        applicationId: null,
-        applicationName: null,
-        applicationCode: null
-    },
-    border: true,
-    stripeRows: true,
-    /*rootProperty:{
-     id:'-1',
-     name:'根功能'
-     },*/
-    manageHeight: true,
-    rootVisible: false,
-    title: '功能列表',
-    iconCls: 'iconfont icon-function-management',
-    columns: {
-        defaults: {flex: 1},
-        items:[
-        {text: '编号', dataIndex: 'id', hidden: true},
-        {text: '上级功能', dataIndex: 'parentName', hidden: true},
-        {text: '权限代码', dataIndex: 'permission', hidden: true},
-        {xtype: 'treecolumn', text: '名称', dataIndex: 'name'},
-        {text: '功能代码', dataIndex: 'code'},
-        {text: '创建人', dataIndex: 'createBy'},
-        {
-            text: '创建日期', dataIndex: 'creationDate'
-        },
-        /*{text: '更新人', dataIndex: 'updateBy'},
-        {
-            text: '更新日期', dataIndex: 'updateDate', renderer: function (value) {
-            var updateDate = new Date(value);
-            return updateDate.format("yyyy-MM-dd hh:mm:ss");
-        }
-        },*/
-        {
-            header: '操作',
-            flex:0.5,
-            xtype: "actioncolumn",
-            items: [{
-                iconCls: 'iconfont icon-edit-column',
-                permission: 'admin:sysModule:functionMenu:edit',
-                tooltip: '编辑',
-                handler: 'onEdit',
-                isDisabled: function (view, rowIdx, colIdx, item, record) {
-                    return record.data.name == "根功能" ? true : false;
-                }
-            }, {
-                iconCls: 'iconfont icon-delete',
-                permission: 'admin:sysModule:functionMenu:delete',
-                tooltip: '删除',
-                handler: 'onDelete',
-                isDisabled: function (view, rowIdx, colIdx, item, record) {
-                    return record.data.name == "根功能" ? true : false;
-                }
-
-            }]
-        }
-    ]},
-    tbar: [
-        {
-            text: '添加',
-            iconCls: 'iconfont icon-add',
-            permission: 'admin:sysModule:functionMenu:add',
-            handler: 'onAdd'
-        }, {
-            text: '刷新',
-            iconCls:'iconfont icon-refresh',
-            permission: 'admin:sysModule:functionMenu:refresh',
-            handler: 'onRefresh'
-        }]
-
+  extend: 'Ext.tree.Panel',
+  requires: [
+    'kalix.view.components.common.SecurityToolbar',
+    'kalix.view.components.common.SecurityGridColumnRUD',
+    'kalix.admin.function.viewModel.FunctionViewModel',
+    'kalix.admin.function.controller.FunctionGridController',
+    'kalix.plugin.AutoHeightPlugin'
+  ],
+  plugins: ['autoheightplugin'],
+  alias: 'widget.functionGrid',
+  xtype: 'functionGridPanel',
+  controller: 'functionGridController',
+  viewModel: {
+    type: 'functionViewModel'
+  },
+  data: {
+    applicationId: null,
+    applicationName: null,
+    applicationCode: null
+  },
+  border: true,
+  stripeRows: true,
+  /*rootProperty:{
+   id:'-1',
+   name:'根功能'
+   },*/
+  manageHeight: true,
+  rootVisible: false,
+  title: '功能列表',
+  iconCls: 'iconfont icon-function-management',
+  columns: {
+    defaults: {flex: 1},
+    items: [
+      {text: '编号', dataIndex: 'id', hidden: true},
+      {text: '上级功能', dataIndex: 'parentName', hidden: true},
+      {text: '权限代码', dataIndex: 'permission', hidden: true},
+      {xtype: 'treecolumn', text: '名称', dataIndex: 'name'},
+      {text: '功能代码', dataIndex: 'code'},
+      {text: '创建人', dataIndex: 'createBy'},
+      {
+        text: '创建日期', dataIndex: 'creationDate'
+      },
+      {
+        xtype: 'securityGridColumnRUD',
+        permissions: ['edit', 'delete']
+      }
+    ]
+  },
+  tbar: {
+    xtype: 'securityToolbar',
+    verifyItems: [
+      {
+        text: '添加',
+        iconCls: 'iconfont icon-add',
+        permission: 'add',
+        handler: 'onAdd'
+      }, {
+        text: '刷新',
+        iconCls: 'iconfont icon-refresh',
+        permission: '',
+        handler: 'onRefresh'
+      }
+    ]
+  }
 });
