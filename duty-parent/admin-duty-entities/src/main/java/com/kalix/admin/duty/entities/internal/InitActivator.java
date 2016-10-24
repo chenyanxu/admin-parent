863c1123fe6c74b42501499e8e6a464024b1388c
chenyanxu <yanxu2016>
2016-10-24 08:09:31 +0800

    use base bundle activator

--- duty-parent/admin-duty-entities/src/main/java/com/kalix/admin/duty/entities/internal/InitActivator.java
+++ duty-parent/admin-duty-entities/src/main/java/com/kalix/admin/duty/entities/internal/InitActivator.java
@@ -2,14 +2,13 @@ package com.kalix.admin.duty.entities.internal;
 
 import com.kalix.admin.duty.entities.DutyBean;
 import com.kalix.admin.duty.entities.DutyUserBean;
-import com.kalix.framework.core.impl.system.BundleActivatorImpl;
-import org.osgi.framework.BundleActivator;
+import com.kalix.framework.core.api.osgi.CascadeBundleActivator;
 import org.osgi.framework.BundleContext;
 
 /**
  * Created by sunlf on 14-3-23.
  */
-public class InitActivator extends BundleActivatorImpl implements BundleActivator {
+public class InitActivator extends CascadeBundleActivator {
     @Override
     public void start(BundleContext bundleContext) throws Exception {
         super.start(bundleContext);
