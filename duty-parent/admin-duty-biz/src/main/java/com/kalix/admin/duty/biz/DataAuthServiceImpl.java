4d005288f0cef3afc32df76ad1ef96d6ce7e8502
sunlingfeng <minikiller@qq.com>
2016-12-03 01:08:54 +0800

    增加数据权限的控制

---
+++ duty-parent/admin-duty-biz/src/main/java/com/kalix/admin/duty/biz/DataAuthServiceImpl.java
@@ -0,0 +1,13 @@
+package com.kalix.admin.duty.biz;
+
+import com.kalix.framework.core.api.security.IDataAuthService;
+
+/**
+ * Created by admin on 2016/12/3.
+ */
+public class DataAuthServiceImpl implements IDataAuthService {
+    @Override
+    public boolean isAuth(String entityClassName, Long userId) {
+        return false;
+    }
+}
