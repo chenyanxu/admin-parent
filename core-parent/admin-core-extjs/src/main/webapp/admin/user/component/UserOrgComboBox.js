3f2aad0232d9724c9e0a707625865177eee30e31
zangyanming <zyanming2002@163.com>
2016-12-20 08:48:22 +0800

    解决userOrgComboBox控件查询的问题

--- core-parent/admin-core-extjs/src/main/webapp/admin/user/component/UserOrgComboBox.js
+++ core-parent/admin-core-extjs/src/main/webapp/admin/user/component/UserOrgComboBox.js
@@ -6,46 +6,48 @@
  * @version 1.0.0
  */
 Ext.define('kalix.admin.user.component.UserOrgComboBox', {
-  extend: 'Ext.form.field.ComboBox',
-  requires: [
-    'kalix.admin.user.store.UserOrgStore'
-  ],
-  alias: 'widget.userOrgComboBox',
-  allowBlank: false,
-  labelAlign: 'right',
-  xtype: 'userOrgComboBox',
-  queryMode: 'local',
-  valueField: 'id',
-  displayField: 'name',
-  queryParam: false,
-  minChars: 0,
-  typeAhead: false,
-  editable: false,
-  store: {
-    type: 'userOrgStore'
-  },
-  constructor: function () {
-    this.callParent(arguments);
+    extend: 'Ext.form.field.ComboBox',
+    requires: [
+        'kalix.admin.user.store.UserOrgStore'
+    ],
+    alias: 'widget.userOrgComboBox',
+    allowBlank: false,
+    labelAlign: 'right',
+    xtype: 'userOrgComboBox',
+    queryMode: 'local',
+    valueField: 'id',
+    displayField: 'name',
+    queryParam: false,
+    minChars: 0,
+    typeAhead: false,
+    editable: false,
+    store: {
+        type: 'userOrgStore'
+    },
+    constructor: function () {
+        this.callParent(arguments);
 
-    this.store.on('load', function (target, records) {
-      if (records.length > 0) {
-        if (records.length == 1) {
-          this.select(records[0]);
-          var recModel = this.lookupViewModel().get('rec');
-          recModel.set('orgName', records[0].get('name'));
-          recModel.modified = {};
-          recModel.dirty=false;
-        }
-      }
-      else {
-        kalix.Notify.warning(CONFIG.ALTER_TITLE_FAILURE, "未找到该用户的部门，请为该用户选择部门！");
-      }
-    }, this);
-    this.on('change', function (target, newValue, oldValue) {
-      if (oldValue || target.store.data.length>1) {
-        this.lookupViewModel().get('rec').set('orgName', target.rawValue);
-      }
-    });
-  }
+        this.store.on('load', function (target, records) {
+            if (records.length > 0) {
+                if (records.length == 1) {
+                    this.select(records[0]);
+                    var recModel = this.lookupViewModel().get('rec');
+                    recModel.set('orgName', records[0].get('name'));
+                    recModel.modified = {};
+                    recModel.dirty = false;
+                }
+            }
+            else {
+                kalix.Notify.warning(CONFIG.ALTER_TITLE_FAILURE, "未找到该用户的部门，请为该用户选择部门！");
+            }
+        }, this);
+        this.on('change', function (target, newValue, oldValue) {
+            if (oldValue || target.store.data.length > 1) {
+                if (this.lookupViewModel().get('rec')) {
+                    this.lookupViewModel().get('rec').set('orgName', target.rawValue);
+                }
+            }
+        });
+    }
 
 });
