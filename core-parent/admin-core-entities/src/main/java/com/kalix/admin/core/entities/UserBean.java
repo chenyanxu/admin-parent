aa0622703d3b1e725bd688d88e34636d51c8ffe9
chenyanxu <yanxu2016>
2017-03-01 17:03:48 +0800

    extend from UserEntity

--- core-parent/admin-core-entities/src/main/java/com/kalix/admin/core/entities/UserBean.java
+++ core-parent/admin-core-entities/src/main/java/com/kalix/admin/core/entities/UserBean.java
@@ -1,6 +1,7 @@
 package com.kalix.admin.core.entities;
 
 import com.kalix.framework.core.api.persistence.PersistentEntity;
+import com.kalix.framework.core.api.persistence.UserEntity;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
@@ -19,105 +20,105 @@ import java.util.Date;
 @Entity
 @Table(name = "sys_user")
 @ApiModel("用户<br>UserBean")
-public class UserBean extends PersistentEntity {
+public class UserBean extends UserEntity {
     @ApiModelProperty(value="工号",position=0,example = "0")
     private Long code;
     @ApiModelProperty(value="岗位",position=1,example = "0")
     private Integer position;
-    @ApiModelProperty(value="性别（男 女）",allowableValues = "男,女",position=2,example = "男")
-    private String sex;
-    @ApiModelProperty(value="登录名",position=3,example = "test_login")
-    private String loginName;
-    @ApiModelProperty(value="姓名",position =4,example = "陈某")
-    private String name;
-    @ApiModelProperty(value="密码",position = 5,example = "123")
-    private String password;
-    @ApiModelProperty(value="邮箱",position=6,example = "text@kalix.ocm")
-    private String email;
-    @ApiModelProperty(value="固定电话",position=7,example = "043288888888")
-    private String phone;
-    @ApiModelProperty(value="手机",position = 8,example = "18866667777")
-    private String mobile;
-    @ApiModelProperty(value="头像地址",position = 9,example = "http://head.png")
-    private String icon;
-    @ApiModelProperty(value="是否可用：（0-不可用 1-可用）",allowableValues = "0,1",position = 10,example = "1")
-    private Long available = 1L;
-    @ApiModelProperty(value="最后登陆IP",hidden=true)
-    private String loginIp;
-    @ApiModelProperty(value="最后登陆日期",hidden = true)
-    private Date loginDate;
-
-    public String getLoginName() {
-        return loginName;
-    }
-
-    public void setLoginName(String loginName) {
-        this.loginName = loginName;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getMobile() {
-        return mobile;
-    }
-
-    public void setMobile(String mobile) {
-        this.mobile = mobile;
-    }
-
-    public String getLoginIp() {
-        return loginIp;
-    }
-
-    public void setLoginIp(String loginIp) {
-        this.loginIp = loginIp;
-    }
-
-    public Date getLoginDate() {
-        return loginDate;
-    }
-
-    public void setLoginDate(Date loginDate) {
-        this.loginDate = loginDate;
-    }
-
-    public Long getAvailable() {
-        return available;
-    }
-
-    public void setAvailable(Long available) {
-        this.available = available;
-    }
+//    @ApiModelProperty(value="性别（男 女）",allowableValues = "男,女",position=2,example = "男")
+//    private String sex;
+//    @ApiModelProperty(value="登录名",position=3,example = "test_login")
+//    private String loginName;
+//    @ApiModelProperty(value="姓名",position =4,example = "陈某")
+//    private String name;
+//    @ApiModelProperty(value="密码",position = 5,example = "123")
+//    private String password;
+//    @ApiModelProperty(value="邮箱",position=6,example = "text@kalix.ocm")
+//    private String email;
+//    @ApiModelProperty(value="固定电话",position=7,example = "043288888888")
+//    private String phone;
+//    @ApiModelProperty(value="手机",position = 8,example = "18866667777")
+//    private String mobile;
+//    @ApiModelProperty(value="头像地址",position = 9,example = "http://head.png")
+//    private String icon;
+//    @ApiModelProperty(value="是否可用：（0-不可用 1-可用）",allowableValues = "0,1",position = 10,example = "1")
+//    private Long available = 1L;
+//    @ApiModelProperty(value="最后登陆IP",hidden=true)
+//    private String loginIp;
+//    @ApiModelProperty(value="最后登陆日期",hidden = true)
+//    private Date loginDate;
+//
+//    public String getLoginName() {
+//        return loginName;
+//    }
+//
+//    public void setLoginName(String loginName) {
+//        this.loginName = loginName;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public String getPhone() {
+//        return phone;
+//    }
+//
+//    public void setPhone(String phone) {
+//        this.phone = phone;
+//    }
+//
+//    public String getMobile() {
+//        return mobile;
+//    }
+//
+//    public void setMobile(String mobile) {
+//        this.mobile = mobile;
+//    }
+//
+//    public String getLoginIp() {
+//        return loginIp;
+//    }
+//
+//    public void setLoginIp(String loginIp) {
+//        this.loginIp = loginIp;
+//    }
+//
+//    public Date getLoginDate() {
+//        return loginDate;
+//    }
+//
+//    public void setLoginDate(Date loginDate) {
+//        this.loginDate = loginDate;
+//    }
+//
+//    public Long getAvailable() {
+//        return available;
+//    }
+//
+//    public void setAvailable(Long available) {
+//        this.available = available;
+//    }
 
     public Integer getPosition() {
         return position;
@@ -126,15 +127,15 @@ public class UserBean extends PersistentEntity {
     public void setPosition(Integer position) {
         this.position = position;
     }
-
-    public String getSex() {
-        return sex;
-    }
-
-    public void setSex(String sex) {
-        this.sex = sex;
-    }
-
+//
+//    public String getSex() {
+//        return sex;
+//    }
+//
+//    public void setSex(String sex) {
+//        this.sex = sex;
+//    }
+//
     public Long getCode() {
         return code;
     }
@@ -142,12 +143,12 @@ public class UserBean extends PersistentEntity {
     public void setCode(Long code) {
         this.code = code;
     }
-
-    public String getIcon() {
-        return icon;
-    }
-
-    public void setIcon(String icon) {
-        this.icon = icon;
-    }
+//
+//    public String getIcon() {
+//        return icon;
+//    }
+//
+//    public void setIcon(String icon) {
+//        this.icon = icon;
+//    }
 }
