1b5bbd5598526328e007e9cf8b9e66931a2a4164
chenyanxu <yanxu2016>
2017-03-01 17:04:28 +0800

    new impl extend from  LoginService

--- core-parent/admin-core-biz/src/main/java/com/kalix/admin/core/biz/UserLoginServiceImpl.java
+++ core-parent/admin-core-biz/src/main/java/com/kalix/admin/core/biz/UserLoginServiceImpl.java
@@ -1,113 +1,104 @@
 package com.kalix.admin.core.biz;
 
 import com.kalix.admin.core.api.biz.IPermissionService;
-import com.kalix.admin.core.api.dao.IUserBeanDao;
-import com.kalix.admin.core.entities.UserBean;
-import com.kalix.framework.core.api.ErrorCodeValue;
-import com.kalix.framework.core.api.security.IUserLoginService;
-import com.kalix.framework.core.util.MD5Util;
-import org.apache.shiro.authc.IncorrectCredentialsException;
-import org.apache.shiro.authc.LockedAccountException;
-import org.apache.shiro.authc.UnknownAccountException;
-
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import com.kalix.framework.core.api.persistence.UserEntity;
+import com.kalix.framework.core.impl.security.LoginService;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 
 /**
  * Created by dell on 14-3-25.
  */
 
-public class UserLoginServiceImpl implements IUserLoginService {
-    private IUserBeanDao userBeanDao;
-
+public class UserLoginServiceImpl extends LoginService{ //implements ILoginService {
+    //private IUserBeanDao userBeanDao;
     private IPermissionService permissionService;
 
-    /**
-     * 设置cookie
-     *
-     * @param response
-     * @param name
-     * @param value
-     * @param maxAge
-     */
-    private static void setCookie(HttpServletResponse response, String name,
-                                  String value, int maxAge) {
-        if (value == null) {
-            value = "";
-        }
-        Cookie cookie = new Cookie(name, value);
-        cookie.setMaxAge(maxAge);
-        cookie.setPath("/");
-        response.addCookie(cookie);
-    }
+//    /**
+//     * 设置cookie
+//     *
+//     * @param response
+//     * @param name
+//     * @param value
+//     * @param maxAge
+//     */
+//    private static void setCookie(HttpServletResponse response, String name,
+//                                  String value, int maxAge) {
+//        if (value == null) {
+//            value = "";
+//        }
+//        Cookie cookie = new Cookie(name, value);
+//        cookie.setMaxAge(maxAge);
+//        cookie.setPath("/");
+//        response.addCookie(cookie);
+//    }
 
-
-    public boolean validateUserStatus(String username) {
-        UserBean user = userBeanDao.getUser(username);
-        if (user == null) {
-            throw new UnknownAccountException("Unknown Account Exception!");
-        }
-        if (user.getAvailable() == 0) {
-            throw new LockedAccountException("Unknown Account Exception!");
-        }
-        return true;
-    }
-
-    @Override
-    public Map login(String username, String password) {
-        Map map = new HashMap();
-        int result = -1;
-
-        UserBean user = userBeanDao.getUser(username);
-        if (user == null) {
-            throw new UnknownAccountException("Unknown Account Exception!");
-        }
-        if (user.getAvailable() != 1L) {
-            throw new LockedAccountException("Account LockedException!");
-        }
-        //判断密码和用户类型是否对应
-        if (encrypt(password).equals(user.getPassword())) {
-            Map resMap = new HashMap();
-            resMap.put("user_id", user.getId());
-            resMap.put("name", user.getName());
-            resMap.put("user_name", user.getLoginName());
-            resMap.put("password", user.getPassword());
-            resMap.put("user_icon", user.getIcon());
-            map.put("response", resMap);
-
-        } else {
-            throw new IncorrectCredentialsException("Incorrect Credentials Exception!");
-        }
-
-        return map;
-
-    }
-
-    @Override
-    public void updateUserLoginInfo(long id, String loginIp) {
-        userBeanDao.updateUserLoginInfo(id, loginIp);
-    }
+//    @Override
+//    public boolean validateUserStatus(String username) {
+//        UserBean user = userBeanDao.getUser(username);
+//
+//        if (user == null) {
+//            throw new UnknownAccountException("Unknown Account Exception!");
+//        }
+//        if (user.getAvailable() == 0) {
+//            throw new LockedAccountException("Unknown Account Exception!");
+//        }
+//
+//        return true;
+//    }
+//
+//    @Override
+//    public Map login(String username, String password) {
+//        Map map = new HashMap();
+//        int result = -1;
+//
+//        UserBean user = userBeanDao.getUser(username);
+//        if (user == null) {
+//            throw new UnknownAccountException("Unknown Account Exception!");
+//        }
+//        if (user.getAvailable() != 1L) {
+//            throw new LockedAccountException("Account LockedException!");
+//        }
+//        //判断密码和用户类型是否对应
+//        if (MD5Util.encode(password).equals(user.getPassword())) {
+//            Map resMap = new HashMap();
+//            resMap.put("user_id", user.getId());
+//            resMap.put("name", user.getName());
+//            resMap.put("user_name", user.getLoginName());
+//            resMap.put("password", user.getPassword());
+//            resMap.put("user_icon", user.getIcon());
+//            map.put("response", resMap);
+//
+//        } else {
+//            throw new IncorrectCredentialsException("Incorrect Credentials Exception!");
+//        }
+//
+//        return map;
+//
+//    }
+//
+//    @Override
+//    public void updateUserLoginInfo(long id, String loginIp) {
+//        userBeanDao.updateUserLoginInfo(id, loginIp);
+//    }
 
     @Override
     public List<String> getUserPermission(String username) {
         List<String> stringList = new ArrayList<String>();
-        UserBean userBean = userBeanDao.getUser(username);
+        UserEntity userBean = dao.getUser(username);
+
         if (userBean != null) {
             stringList.addAll(permissionService.getApplicationCodesByUserId(userBean.getId()));
             stringList.addAll(permissionService.getFunctionCodesByUserId(userBean.getId()));
         }
+
         return stringList;
     }
 
-    private String encrypt(String text) {
-        return MD5Util.encode(text);
-    }
+//    private String encrypt(String text) {
+//        return MD5Util.encode(text);
+//    }
 
 //    @Override
 //    public Map loginByPhone(String username, String password, String client,
@@ -135,30 +126,30 @@ public class UserLoginServiceImpl implements IUserLoginService {
 //        return map;
 //    }
 
-    private HashMap logon(UserBean user, String client, HttpServletRequest request, HttpServletResponse response) throws Exception {
-        int rs = 1;
-        HashMap rsMap = new HashMap();
-        try {
-            String map_provide = "1";//先设成1 用时再改
-            //String value = user.getId()+"::"+user.getLoginName()+"::"+user.getName()+"::"+map_provide+"::"+client;
-            String value = user.getId() + "::" + user.getLoginName();
-            int maxAge;
-            if (client.equals("0")) {
-                maxAge = 24 * 60 * 60;
-            } else {
-                maxAge = 60 * 24 * 60 * 60;
-            }
-            //String key = UtilTools.generateKey().toString();
-            //value = UtilTools.encryptString(key, value);
-            //value = URLEncoder.encode(value + "||" + key, "UTF-8");
-            setCookie(response, "jldaren.cooperate", value, maxAge);
-            rsMap.put("result", rs);
-            rsMap.put("user", user);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return rsMap;
-    }
+//    private HashMap logon(UserBean user, String client, HttpServletRequest request, HttpServletResponse response) throws Exception {
+//        int rs = 1;
+//        HashMap rsMap = new HashMap();
+//        try {
+//            String map_provide = "1";//先设成1 用时再改
+//            //String value = user.getId()+"::"+user.getLoginName()+"::"+user.getName()+"::"+map_provide+"::"+client;
+//            String value = user.getId() + "::" + user.getLoginName();
+//            int maxAge;
+//            if (client.equals("0")) {
+//                maxAge = 24 * 60 * 60;
+//            } else {
+//                maxAge = 60 * 24 * 60 * 60;
+//            }
+//            //String key = UtilTools.generateKey().toString();
+//            //value = UtilTools.encryptString(key, value);
+//            //value = URLEncoder.encode(value + "||" + key, "UTF-8");
+//            setCookie(response, "jldaren.cooperate", value, maxAge);
+//            rsMap.put("result", rs);
+//            rsMap.put("user", user);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return rsMap;
+//    }
 
 //    @Override
 //    public Map updateToken(HttpServletRequest request, String token, Long user_id) {
@@ -179,9 +170,9 @@ public class UserLoginServiceImpl implements IUserLoginService {
 //        return map;
 //    }
 
-    public void setUserBeanDao(IUserBeanDao userBeanDao) {
-        this.userBeanDao = userBeanDao;
-    }
+//    public void setUserBeanDao(IUserBeanDao userBeanDao) {
+//        this.userBeanDao = userBeanDao;
+//    }
 
     public void setPermissionService(IPermissionService permissionService) {
         this.permissionService = permissionService;
