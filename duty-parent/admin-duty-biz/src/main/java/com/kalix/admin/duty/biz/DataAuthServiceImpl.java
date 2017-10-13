37a85965618572a988bfd68752993d77bced512c
minikiller <minikiller@qq.com>
2017-10-13 10:43:08 +0800

    增加数据权限code

--- duty-parent/admin-duty-biz/src/main/java/com/kalix/admin/duty/biz/DataAuthServiceImpl.java
+++ duty-parent/admin-duty-biz/src/main/java/com/kalix/admin/duty/biz/DataAuthServiceImpl.java
@@ -1,6 +1,9 @@
 package com.kalix.admin.duty.biz;
 
 import com.kalix.framework.core.api.security.IDataAuthService;
+import com.kalix.framework.core.api.security.model.EnumDataAuth;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.session.Session;
 
 /**
  * Created by admin on 2016/12/3.
@@ -9,5 +12,17 @@ public class DataAuthServiceImpl implements IDataAuthService {
     @Override
     public boolean isAuth(String entityClassName, Long userId) {
         return false;
+    }
+
+    @Override
+    public EnumDataAuth getDataAuth(Long userId) {
+        Session session = SecurityUtils.getSubject().getSession();
+        String reqAppName = session.getAttribute("DataAuthApp").toString();
+        if (reqAppName.equals(""))
+            return EnumDataAuth.ALL;
+        else {
+            //根据appName查询具体的数据权限
+        }
+        return EnumDataAuth.ALL;
     }
 }
