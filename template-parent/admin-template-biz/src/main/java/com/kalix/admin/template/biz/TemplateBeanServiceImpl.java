d01ff3260c8b11532f8afbac7f3611946177a38e
houqingjie <150091225@qq.com>
2018-01-31 09:35:41 +0800

    修改模板服务bug(包括新建模板保存和使用模板)

--- template-parent/admin-template-biz/src/main/java/com/kalix/admin/template/biz/TemplateBeanServiceImpl.java
+++ template-parent/admin-template-biz/src/main/java/com/kalix/admin/template/biz/TemplateBeanServiceImpl.java
@@ -4,15 +4,15 @@ package com.kalix.admin.template.biz;
 import com.kalix.admin.template.api.biz.ITemplateBeanService;
 import com.kalix.admin.template.api.dao.ITemplateBeanDao;
 import com.kalix.admin.template.entities.TemplateBean;
-import com.kalix.framework.core.api.exception.KalixRuntimeException;
-import com.kalix.framework.core.api.persistence.JsonData;
 import com.kalix.framework.core.api.persistence.JsonStatus;
 import com.kalix.framework.core.impl.biz.GenericBizServiceImpl;
-import com.kalix.framework.core.util.SerializeUtil;
 import com.kalix.framework.core.util.StringUtils;
 
 import javax.transaction.Transactional;
-import java.util.*;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 /**
  * @类描述：公告管理
@@ -23,6 +23,7 @@ import java.util.*;
  * @修改备注：
  */
 public class TemplateBeanServiceImpl extends GenericBizServiceImpl<ITemplateBeanDao, TemplateBean> implements ITemplateBeanService {
+    private static final String FUNCTION_NAME = "模板信息";
     private JsonStatus jsonStatus = new JsonStatus();
     private String uuid;
 
@@ -37,23 +38,16 @@ public class TemplateBeanServiceImpl extends GenericBizServiceImpl<ITemplateBean
     }
 
     @Override
-    public JsonStatus saveEntity(TemplateBean entity) {
-        JsonStatus jsonStatus = new JsonStatus();
-        Map<String, String> params = new HashMap();
-        params.put("name", entity.getName());
-        String jsonStr = SerializeUtil.serializeJson(params);
-
-        // 根据模板名称查找模板，判断该模板名称是否已经存在，如果存在给前台提示信息
-        JsonData jsonData = super.getAllByNativeQuery(0, 10, jsonStr);
-        List<TemplateBean> templateBeanList = jsonData.getData();
-        if (templateBeanList != null && templateBeanList.size() > 0) {
-            jsonStatus.setFailure(true);
-            jsonStatus.setMsg("该模板名称已经存在，请修改！");
-        } else {
-            jsonStatus = super.saveEntity(entity);
+    @Transactional
+    public boolean isSave(TemplateBean entity, JsonStatus status) {
+        TemplateBean bean = (TemplateBean) entity;
+        List<TemplateBean> templateBeans = dao.find("select ob from TemplateBean ob where ob.name = ?1", bean.getName());
+        if (templateBeans != null && templateBeans.size() > 0) {
+            status.setFailure(true);
+            status.setMsg(FUNCTION_NAME + "已经存在！");
+            return false;
         }
-
-        return jsonStatus;
+        return true;
     }
 
     /**
@@ -62,34 +56,28 @@ public class TemplateBeanServiceImpl extends GenericBizServiceImpl<ITemplateBean
      * @param templateMap   该模板的项目
      * @return String       替换后的模板
      * */
-
     public String getTemplateResult(String templateName, Map<String, String> templateMap) {
+
         String result = "";
-        if (StringUtils.isEmpty(templateName) || templateMap == null || templateMap.size() < 1) {
+        if (StringUtils.isEmpty(templateName)) {
             return result;
         }
 
-        Map<String, String> params = new HashMap();
-        params.put("name", templateName);
-        String jsonStr = SerializeUtil.serializeJson(params);
-
-        JsonData jsonData = super.getAllByNativeQuery(0, 10, jsonStr);
-        List<TemplateBean> templateBeanList = jsonData.getData();
-        if (templateBeanList != null && templateBeanList.size() > 0) {
-            String template = templateBeanList.get(0).getContent();
-
-            String key,value;
-            Iterator<Map.Entry<String, String>> it = templateMap.entrySet().iterator();
-            while (it.hasNext()) {
-                Map.Entry<String, String> entry = it.next();
-                key = "${" + entry.getKey() + "}";
-                value = entry.getValue();
-                result = template;
-                result = result.replaceAll(key,value);
+        List<TemplateBean> templateBeans = dao.find("select ob from TemplateBean ob where ob.name = ?1", templateName);
+        if (templateBeans != null && templateBeans.size() > 0) {
+            result = templateBeans.get(0).getContent();
+            if (templateMap != null && templateMap.size() > 0) {
+                String key, value;
+                Iterator<Map.Entry<String, String>> it = templateMap.entrySet().iterator();
+                while (it.hasNext()) {
+                    Map.Entry<String, String> entry = it.next();
+                    key = "${" + entry.getKey() + "}";
+                    value = entry.getValue();
+                    result = result.replaceAll(key, value);
+                }
             }
         }
 
         return result;
     }
-
 }
