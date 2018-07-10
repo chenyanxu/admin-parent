62936725a4a74550c80d85e2267bafbc674c8259
houqingjie <150091225@qq.com>
2018-07-11 07:53:12 +0800

    修改config.admin.dashboard配置文件及服务

--- core-parent/admin-core-biz/src/main/java/com/kalix/admin/core/biz/AdminDashboardServiceImpl.java
+++ core-parent/admin-core-biz/src/main/java/com/kalix/admin/core/biz/AdminDashboardServiceImpl.java
@@ -1,10 +1,7 @@
 package com.kalix.admin.core.biz;
 
 import com.kalix.framework.core.api.biz.IDashboardService;
-import com.kalix.framework.core.api.dto.PieSeriesDataDTO;
-import com.kalix.framework.core.impl.biz.DashboardServiceImpl;
-
-import java.util.List;
+import com.kalix.framework.core.impl.biz.BaseDashboardServiceImpl;
 
 /**
  * 项目名称:
@@ -15,30 +12,5 @@ import java.util.List;
  * 修改时间:
  * 修改备注:  [说明本次修改内容]
  */
-public class AdminDashboardServiceImpl extends DashboardServiceImpl implements IDashboardService {
-
-    @Override
-    public Integer getPanelGroupBizData(String chartKey) {
-        return super.getPanelGroupBizData(chartKey);
-    }
-
-    @Override
-    public List<Integer> getLineChartBizData(String chartKey, String legend) {
-        return super.getLineChartBizData(chartKey, legend);
-    }
-
-    @Override
-    public List<Integer> getRaddarChartBizData(String chartKey, String legend) {
-        return super.getRaddarChartBizData(chartKey, legend);
-    }
-
-    @Override
-    public List<PieSeriesDataDTO> getPieChartBizData(String chartKey) {
-        return super.getPieChartBizData(chartKey);
-    }
-
-    @Override
-    public List<Integer> getBarChartBizData(String chartKey, String legend) {
-        return super.getBarChartBizData(chartKey, legend);
-    }
+public class AdminDashboardServiceImpl extends BaseDashboardServiceImpl implements IDashboardService {
 }
