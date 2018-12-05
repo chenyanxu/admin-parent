7908cd55a628b575a5f0b9909a9e307bd3a6ef28
yangze8818 <yangze_lmkj@163.com>
2018-12-05 09:23:39 +0800

    UUID环境Shiro-session Oauth2问题修改

--- duty-parent/admin-duty-biz/src/main/java/com/kalix/admin/duty/biz/DataAuthServiceImpl.java
+++ duty-parent/admin-duty-biz/src/main/java/com/kalix/admin/duty/biz/DataAuthServiceImpl.java
@@ -26,35 +26,38 @@ public class DataAuthServiceImpl implements IDataAuthService {
     @Override
     public EnumDataAuth getDataAuth(String userId) {
         ISystemService systemService = null;
-        Session session = SecurityUtils.getSubject().getSession();
-        String reqAppName = session.getAttribute("DataAuthApp").toString();
-        if (!reqAppName.equals("")) {
-            try {
-                reqAppName = reqAppName.trim();
-                if (reqAppName.substring(0, 1).equals("/")) {
-                    reqAppName = reqAppName.substring(1);
+        Session session = SecurityUtils.getSubject().getSession(false);
+        if (session != null) {
+            System.out.println("EnumDataAuth session id:" + session.getId());
+            String reqAppName = session.getAttribute("DataAuthApp").toString();
+            if (!reqAppName.equals("")) {
+                try {
+                    reqAppName = reqAppName.trim();
+                    if (reqAppName.substring(0, 1).equals("/")) {
+                        reqAppName = reqAppName.substring(1);
+                    }
+                    if (reqAppName.substring(reqAppName.length() - 1, reqAppName.length()).equals("s")) {
+                        reqAppName = reqAppName.substring(0, reqAppName.length() - 1);
+                    }
+                    systemService = JNDIHelper.getJNDIServiceForName(ISystemService.class.getName());
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-                if (reqAppName.substring(reqAppName.length() - 1, reqAppName.length()).equals("s")) {
-                    reqAppName = reqAppName.substring(0, reqAppName.length() - 1);
-                }
-                systemService = JNDIHelper.getJNDIServiceForName(ISystemService.class.getName());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            String appName = systemService.getAppName(reqAppName); //获得appName
-            String menuIdToLower = reqAppName + "menu";
-            if (functionBeanService.getDataAuth(appName, reqAppName)) { // 判断是否设置了数据权限开关
-                //根据appName查询具体的数据权限
-                DataAuthBean authBean = dataAuthBeanService.getDataAuthBean(userId, appName, menuIdToLower);
-                if (authBean == null) {
-                    return EnumDataAuth.SELF;  //设置了数据权限开关，默认为只能查看自己建立的数据
+                String appName = systemService.getAppName(reqAppName); //获得appName
+                String menuIdToLower = reqAppName + "menu";
+                if (functionBeanService.getDataAuth(appName, reqAppName)) { // 判断是否设置了数据权限开关
+                    //根据appName查询具体的数据权限
+                    DataAuthBean authBean = dataAuthBeanService.getDataAuthBean(userId, appName, menuIdToLower);
+                    if (authBean == null) {
+                        return EnumDataAuth.SELF;  //设置了数据权限开关，默认为只能查看自己建立的数据
+                    } else {
+                        return EnumDataAuth.values()[authBean.getType()];
+                    }
                 } else {
-                    return EnumDataAuth.values()[authBean.getType()];
+                    return EnumDataAuth.ALL; //未设置数据权限，则默认为全部数据
                 }
-            } else {
-                return EnumDataAuth.ALL; //未设置数据权限，则默认为全部数据
-            }
 
+            }
         }
         return EnumDataAuth.ALL;
     }
