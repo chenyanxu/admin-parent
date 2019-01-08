6d06cef62f83356a396ea8242af9c24182646ba4
yangze8818 <yangze_lmkj@163.com>
2019-01-08 08:48:45 +0800

    在线考试系统，初始数据

---
+++ core-parent/admin-core-etc/src/main/resources/sys_user-test.sql
@@ -0,0 +1,10 @@
+
+INSERT INTO "public"."sys_user" VALUES ('-2', '系统', null, '2016-08-04 15:13:54.808', '系统', null, '2016-08-04 15:13:54.808', '1', null, null, null, '0:0:0:0:0:0:0:1', 'qwer', null, '操作员', 'ICy5YqxZB1uWSwcVLSNLcA==', null, null, '130', null, '-1', null);
+INSERT INTO "public"."sys_user" VALUES ('-1', '系统', null, null, '系统', null, null, '1', null, null, '2018-12-25 15:54:39.763', '0:0:0:0:0:0:0:1', 'admin', null, '管理员', 'ICy5YqxZB1uWSwcVLSNLcA==', null, null, null, null, '-1', null);
+INSERT INTO "public"."sys_user" VALUES ('120656', '管理员', '-1', '2018-12-25 15:46:05.79', '管理员', '-1', '2018-12-25 15:46:05.768', '1', null, null, null, null, 'teacher1', null, '张三', 'ICy5YqxZB1uWSwcVLSNLcA==', null, '男', '001', '2', '0', '1');
+INSERT INTO "public"."sys_user" VALUES ('120657', '管理员', '-1', '2018-12-25 15:46:33.303', '管理员', '-1', '2018-12-25 15:46:33.295', '1', null, null, null, null, 'teacher2', null, '李四', 'ICy5YqxZB1uWSwcVLSNLcA==', null, null, '002', '2', '0', '1');
+INSERT INTO "public"."sys_user" VALUES ('120658', '管理员', '-1', '2018-12-25 15:48:27.69', '管理员', '-1', '2018-12-25 15:48:27.682', '1', 'wd@qq.com', null, null, null, 'student1', '15878787712', '王迪', 'ICy5YqxZB1uWSwcVLSNLcA==', null, '男', '001', null, '1', '1');
+INSERT INTO "public"."sys_user" VALUES ('120659', '管理员', '-1', '2018-12-25 15:50:51.523', '管理员', '-1', '2018-12-25 15:50:51.514', '1', 'lh@qq.com', null, null, null, 'student2', '15911111111', '刘华', 'ICy5YqxZB1uWSwcVLSNLcA==', null, '女', '002', null, '1', '1');
+
+
+INSERT INTO "public"."sys_organization" ("id", "createby", "createbyid", "creationdate", "updateby", "updatebyid", "updatedate", "areaid", "centercode", "code", "dept", "isleaf", "name", "parentid", "version_") VALUES ('40810', '管理员', NULL, '2016-02-29 13:52:29.641', 'admin', NULL, '2017-11-16 10:00:01.272', '10', NULL, '001', 'f', '0', '吉林动画学院', '-1', '6');
